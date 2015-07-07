667d28b0b6e435f2eaa87cc5629d8c78e4cbb3a9
Zhang Visper <i@zjy.name>
2015-07-07 13:17:13 +0800

    new way to call alphabet

--- README.md
+++ README.md
@@ -13,24 +13,52 @@
 ## ʹ��
 ���� alphabetNav.css �� alphabetNav.js
 
+### 调用方式 1（推荐）
+
+```javascript
+alphabetNav("attrName",{options})
+```
+
+其中，attrName 为属性名，options 为配置项。元素属性名的对应属性值将作为导航显示内容。该调用方式下 URL Hash 跳转特性不可用。
+
+DEMO:
+```html
+<section nav="A">
+<section nav="B">
+……
+<section nav="Z">
+
+<script type="text/javascript">
+alphabet("nav");
+</script>
+```
+
+### 调用方式 2
+
 ```javascript
 alphabetNav([navItem],{options})
 ```
 
-���У�navItem Ϊ�������飬������� display �� id �������ԣ�display Ϊ������ʾ���ƣ�id Ϊ��Ӧҳ��ê������
+其中，navItem 为对象数组，对象包括 display 和 id 两个属性，display 为导航显示名称，id 为对应页面锚点名称。
 
+DEMO:
+```html
+<section id="a">
+<section id="b">
+<section id="c">
+
+<script type="text/javascript">
+alphabet([
+		{display:"A", id:"a"},
+		{display:"B", id:"b"},
+		{display:"C", id:"c"}
+		]);
+</script>
+```
+
+### 配置项
 options 可选配置包括：
 + top：数字或字符串，设置边栏的 top 属性。
 + bottom：数字或字符串，设置边栏的 bottom 属性，top 和 bottom 均没有配置时，slideNav 将居中显示。
 + showIndicator：布尔型，默认值 true，设置是否显示字母指示器。
 + useHash：布尔型，默认值 false，是否使用 URLHash 进行跳转。注意使用 URLHash 进行跳转时，只在 URLHash 改变时跳转才会发生，这将导致某些情况下不能跳转到正确位置。
-
-```javascript
-alphabetNav([{display:"A", id:"a"},
-              {display:"B", id:"b"},
-              {display:"C", id:"c"},
-              {display:"D", id:"d"},
-              {display:"E", id:"e"},
-              {display:"F", id:"f"}],
-			{top:20, showIndicator:true})
-```
--- alphabetNav.js
+++ alphabetNav.js
@@ -1,6 +1,7 @@
 (function(d){
 	var inited = false, // 是否初始化
-		top, bottom, showIndicator = true, useHash = false; // 默认配置
+		top, bottom, showIndicator = true, useHash = false, // 默认配置
+		attrKey;
 	
 	/**
 	 * 初始化方法
@@ -12,7 +13,7 @@
 	 *
 	 */
 	function init(pAnchorList, pOptions){
-		var i, list, listUl, listLi, indicator;
+		var i, list, listUl, listLi, indicator, anchorList;
 		
 		// 如果已经初始化过，提醒并返回
 		if(inited){
@@ -21,8 +22,9 @@
 		}
 		
 		// 检查参数类型
-		if(Object.prototype.toString.call(pAnchorList) !== "[object Array]"){
-			throw new TypeError("first parameter of alphabet must be an Array");
+		if(Object.prototype.toString.call(pAnchorList) !== "[object Array]" &&
+				typeof pAnchorList !== "string"){
+			throw new TypeError("first parameter of alphabet must be an Array or String");
 		}
 		if(typeof pOptions !== "undefined" && typeof pOptions !== "object"){
 			throw new TypeError("second parameter of alphabet must be an Object");
@@ -46,12 +48,29 @@
 		listUl = myCreateElement("ul")
 		list.appendChild(listUl);
 		
-		for(i = 0; i < pAnchorList.length; i++){
-			// 检测数组元素类型
-			if(typeof pAnchorList[i] !== "object") continue;
-			listLi = myCreateElement("li", "alphabet", pAnchorList[i].display);
-			listLi.setAttribute("to", pAnchorList[i].id);
-			listUl.appendChild(listLi);	
+		// 判断调用类型
+		if(typeof pAnchorList === "string"){
+			useHash = false; // 该调用类型下不允许 URL Hash 跳转
+			attrKey = pAnchorList;
+			
+			// 根据传递的参数选择包含指定属性的元素
+			anchorList = d.querySelectorAll("[" + pAnchorList + "]");
+			
+			// 添加导航项
+			for(i = 0; i < anchorList.length; i++){
+				listLi = myCreateElement("li", "alphabet", anchorList[i].getAttribute(attrKey));
+				listUl.appendChild(listLi);
+			}
+		}
+		else{	
+			// 添加导航项
+			for(i = 0; i < pAnchorList.length; i++){
+				// 检测数组元素类型
+				if(typeof pAnchorList[i] !== "object") continue;
+				listLi = myCreateElement("li", "alphabet", pAnchorList[i].display);
+				listLi.setAttribute("to", pAnchorList[i].id);
+				listUl.appendChild(listLi);	
+			}
 		}
 		
 		// 处理位置属性
@@ -109,7 +128,14 @@
 			}
 			// 不使用 url hash 进行跳转
 			else{
-				targetItem = d.getElementById(currentItem.getAttribute("to"));
+				// 属性调用模式
+				if(typeof attrKey !== "undefined"){
+					targetItem = d.querySelector("[" + attrKey + "='" +
+							currentItem.innerText + "']")
+				}
+				else{
+					targetItem = d.getElementById(currentItem.getAttribute("to"));
+				}
 				if(targetItem){
 					d.body.scrollTop += targetItem.getBoundingClientRect().top;
 				}
@@ -126,6 +152,8 @@
 		// 初始化完毕
 		inited = true;
 	}
+	
+
 	
 	function myCreateElement(pTagName, pClassName, pInnerText){
 		var newElement = d.createElement(pTagName);
